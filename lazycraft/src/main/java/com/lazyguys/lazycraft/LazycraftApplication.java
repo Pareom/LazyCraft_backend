b52168c4dfd6f553e007d23343c7ccd35a296a2a
Pareom <yannis.bourree@gmail.com>
2022-12-10 16:01:44 +0100

    Feat(lazycraft): Init project

---
+++ lazycraft/src/main/java/com/lazyguys/lazycraft/LazycraftApplication.java
@@ -0,0 +1,13 @@
+package com.lazyguys.lazycraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazycraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazycraftApplication.class, args);
+	}
+
+}
---
+++ lazycraft/src/test/java/com/lazyguys/lazycraft/LazycraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazyguys.lazycraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazycraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
